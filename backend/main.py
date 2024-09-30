809b1227a941fe920f95abf537d7b121d77540b9
Antonio Jose Mendoza <166577283+anto16jose@users.noreply.github.com>
2024-09-30 07:51:26 -0500

    Add files via upload

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,13 @@
+from fastapi import FastAPI
+from auth import auth_router
+from events import event_router
 
+app = FastAPI()
+
+# Registrar rutas
+app.include_router(auth_router, prefix="/auth")
+app.include_router(event_router, prefix="/events")
+
+@app.get("/")
+def root():
+    return {"message": "Gestión de Eventos con FastAPI"}
